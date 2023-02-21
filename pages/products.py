5d4d4132f513785783d389d7b3ed32add28671ee
DhrobePy <107049606+DhrobePy@users.noreply.github.com>
2023-02-21 17:35:17 +0600

    Update products.py

--- pages/products.py
+++ pages/products.py
@@ -2,13 +2,13 @@ import streamlit as st
 from streamlit_lottie import st_lottie
 import json
 
-st.write("Fuck You")
+st.write("This is where all products goes")
 
-st.sidebar.header("Fuck YOu Too")
+st.sidebar.header("Category")
 
 def load_lottiefile(path:str):
     with open (path,"r")as f:
         return json.load(f)
 
-welcome_animation2=load_lottiefile("/app/ujjal-py/pages/2nd.json")
+welcome_animation2=load_lottiefile("/app/ujjal-py/pages/products.json")
 st_lottie(welcome_animation2)
