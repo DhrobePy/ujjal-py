be45f0f0a9d83c1e1c219d217306c460219bccb8
DhrobePy <107049606+DhrobePy@users.noreply.github.com>
2023-03-23 09:30:20 +0600

    Update Login.py

--- Login.py
+++ Login.py
@@ -23,7 +23,7 @@ hide_st_style = """
             header {visibility: hidden;}
             </style>
             """
-st.markdown(hide_st_style, unsafe_allow_html=True)
+#st.markdown(hide_st_style, unsafe_allow_html=True)
 
 
 # Define username and password
