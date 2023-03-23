fb210c861f08e5f7b3f81a27a28bc7bbce31c262
DhrobePy <107049606+DhrobePy@users.noreply.github.com>
2023-03-23 09:35:43 +0600

    Update Login.py

--- Login.py
+++ Login.py
@@ -16,14 +16,6 @@ db = firestore.client()
 
 import pandas as pd
 
-hide_st_style = """
-            <style>
-            #MainMenu {visibility: hidden;}
-            footer {visibility: hidden;}
-            header {visibility: hidden;}
-            </style>
-            """
-#st.markdown(hide_st_style, unsafe_allow_html=True)
 
 
 # Define username and password
